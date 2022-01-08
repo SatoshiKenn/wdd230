355d974ace11ad672ac3ff3664c634b5793c1a0e
SatoshiKenn <guevara.kennette@gmail.com>
2022-01-08 03:06:15 -0300

    dates.js file added

---
+++ js/dates.js
@@ -0,0 +1,7 @@
+//Last date modified code
+const lastmodified = document.querySelector("#lastmodified");
+lastmodified.textContent = `Last Modified: ${document.lastModified}`;
+
+//Get year code
+const year = document.querySelector("#copyright");
+year.innerHTML = `&copy; Kennette Guevara ${get.year}`;

37594d5d7c092fbfd1660000ea259d27af41948e
SatoshiKenn <guevara.kennette@gmail.com>
2022-01-15 00:52:08 -0300

    lesson 2 updates

--- lesson2/js/dates.js
+++ lesson2/js/dates.js
@@ -1,6 +1,6 @@
 //Last date modified code
 const lastmodified = document.querySelector("#lastmodified");
-lastmodified.textContent = `Last Modified: ${document.lastModified}`;
+lastmodified.textContent = `Last Updated: ${document.lastModified}`;
 
 //Get year code
 const p_year = document.querySelector("#copyright");

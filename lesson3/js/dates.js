e8c0f23d44d2453d012b388c38f8a10ea355ca1e
SatoshiKenn <guevara.kennette@gmail.com>
2022-01-22 23:32:59 -0300

    Lesson 3 update

---
+++ lesson3/js/dates.js
@@ -0,0 +1,9 @@
+//Last date modified code
+const lastmodified = document.querySelector("#lastmodified");
+lastmodified.innerHTML = `Last Updated: ${document.lastModified}`;
+
+//Get year code
+const p_year = document.querySelector("#copyright");
+const today = new Date();
+const year = today.getFullYear();
+p_year.innerHTML = `&copy; ${year} Kennette Guevara`;

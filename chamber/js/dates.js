95b39a7c0cd8ec4318be5564c5ccf9613cdb11f9
SatoshiKenn <guevara.kennette@gmail.com>
2022-02-06 03:01:44 -0300

    major changes to index.html file

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -11,3 +11,36 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 }).format(now);
 
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+/*Join chamber code*/
+const day = now.getDay()
+
+switch (day) {
+  case 0:
+    document.querySelector("#joinchamber").style.display = "none";
+    console.log("Sunday")
+    break;
+  case 1:
+    document.querySelector("#joinchamber").style.display = "block";
+    console.log("Monday")
+    break;
+  case 2:
+    document.querySelector("#joinchamber").style.display = "block";
+    console.log("Tuesday")
+    break;
+  case 3:
+    document.querySelector("#joinchamber").style.display = "none";
+    console.log("Wednesday")
+    break;
+  case 4:
+    document.querySelector("#joinchamber").style.display = "none";
+    console.log("Thursday")
+    break;
+  case 5:
+    document.querySelector("#joinchamber").style.display = "none";
+    console.log("Friday")
+    break;
+  case 6:
+    document.querySelector("#joinchamber").style.display = "none";
+    console.log("Saturday");
+}

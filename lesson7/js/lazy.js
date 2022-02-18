8f99c80ce74c9048d9d4f46a01a12bcf5e5c5767
SatoshiKenn <guevara.kennette@gmail.com>
2022-02-18 01:29:20 -0300

    lesson7 update

---
+++ lesson7/js/lazy.js
@@ -0,0 +1,28 @@
+//get all images
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+//Intersection Observer Check
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }

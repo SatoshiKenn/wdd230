228c7e057fabbb8451d81042c9926aafef151e11
SatoshiKenn <guevara.kennette@gmail.com>
2022-02-20 02:07:42 -0300

    discover page, lazy and windchill js files update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -11,7 +11,15 @@ if (t <= 10 && s > 4.8) {
 // output
 document.querySelector("#w").innerHTML = windchill;
 
+/*Celsius*/
+function windChill(tempC, speed){
+  windchill = (13.2 + 0.6215 * tempC - 11.37 * Math.pow(speed, 0.16) + 0.3965 * tempC * Math.pow(speed, 0.16));
+  return windchill.toFixed(2);
+}
+
+/*Fahrenheit
 function windChill(tempF, speed){
     windchill = (35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16));
     return windchill.toFixed(2);
 }
+*/

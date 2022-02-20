504f50a2b066d0d032ed86176352382879ce746f
SatoshiKenn <guevara.kennette@gmail.com>
2022-02-20 03:34:54 -0300

    visit update

---
+++ chamber/js/visits.js
@@ -0,0 +1,16 @@
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);

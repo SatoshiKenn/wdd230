2f8fa5a4e436cf0b609b58699090f3f18f63bace
SatoshiKenn <guevara.kennette@gmail.com>
2022-03-05 02:40:01 -0300

    directory page update

--- chamber/js/businesses.js
+++ chamber/js/businesses.js
@@ -1,7 +1,7 @@
 const requestURL =
   "https://satoshikenn.github.io/wdd230/chamber/data/data.json";
 
-const bus = document.querySelector(".bus");
+const cards = document.querySelector(".cards");
 
 fetch(requestURL)
   .then(function (response) {
@@ -15,24 +15,26 @@ fetch(requestURL)
 
 function displayBusiness(business) {
   let card = document.createElement("section");
+  card.setAttribute("class", "bcard");
+
   let logo = document.createElement("img");
   let h2 = document.createElement("h2");
   let p = document.createElement("p");
 
   h2.textContent = `${business.name}`;
   p.innerHTML = `${business.address}<br/>
-  ${business.phone}<br>
-    ${business.city},<br/>
+    ${business.city}<br/>
+    ${business.phone}</br>
     ${business.website}`;
 
   logo.setAttribute("src", business.imageurl);
-  logo.setAttribute("class", "blogo");
-  logo.setAttribute("alt", `${business.name}'s Logo`);
+  logo.setAttribute("alt", `${business.name}'s logo`);
   logo.setAttribute("loading", "lazy");
+  logo.setAttribute("class", "blogo");
 
   card.appendChild(logo);
   card.appendChild(h2);
   card.appendChild(p);
 
-  bus.appendChild(card);
+  cards.appendChild(card);
 }

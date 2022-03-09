e3b126a662ccaf83ca514777d472c832aa818716
SatoshiKenn <guevara.kennette@gmail.com>
2022-03-09 17:25:30 -0300

    weather api index update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,17 +1,19 @@
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?id=3871336&appid=3755650b1c53c50d6c91c306ae05d912&units=imperial";
+  "https://api.openweathermap.org/data/2.5/weather?id=3871336&appid=3755650b1c53c50d6c91c306ae05d912&units=metric";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    //document.querySelector("#current-temp").textContent = jsObject.main.temp;
+    document.querySelector("#t").textContent = jsObject.main.temp;
     const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
+    const speed = jsObject.wind.speed;
     //document.querySelector("#icon-src").textContent = iconsrc;
     document.querySelector("#weathericon").setAttribute("src", iconsrc);
     document.querySelector("#weathericon").setAttribute("alt", desc);
-    //document.querySelector("figcaption").textContent = desc;
+    document.querySelector("figcaption").textContent = desc;
+    document.querySelector("#s").textContent = speed;
   });
 
 let t = parseFloat(document.querySelector("#t").textContent);
